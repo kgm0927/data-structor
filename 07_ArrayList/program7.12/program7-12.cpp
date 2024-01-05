56e86315b7b179aa9ef446932a3e13053aec20bd
kgm0927 <initial929@gmail.com>
2024-01-05 15:20:11 +0900

    ArrayList2 까지 완료.

--- 07_ArrayList/program7.12/program7-12.cpp
+++ 07_ArrayList/program7.12/program7-12.cpp
@@ -1,29 +1,75 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef int element;
+//
+// 
+//				7.6 연결 리스트로 구현한 큐
+// 
+// 연결 리스트를 이용하여 스택을 만들 수 있는 것처럼 큐도 연결 리스트를 이용하여 만들 수
+// 있다. 연결리스트로 만들어진 큐를 '연결된 큐'(linked queue)라고 한다. 연결 리스트로 구현된
+// 큐는 배열로 구현된 큐에 비하여 크기가 제한되지 않는다는 장점을 가지고 있다. 반면 배열로
+// 구현되 큐에 비하여 코드가 약간 더 복잡해지고, 링크 필드 때문에 메모리 공간을 더 많이 사용한다.
+// 
+// 기본적인 구조는 단순 연결 리스트에다가 2개의 포인터를 추가한 것과 같다. front 포인터는 삭제와
+// 관련하여 rear 포인터는 삽입과 관련한다. front는 연결 리스트의 맨 앞에 있는 요소를 가리키며,
+// rear 포인터는 맨 뒤에 있는 요소를 가리킨다. 큐에 요소가 없는 경우에는 front와 rear는 NULL값이
+// 된다.
+//
+//
 
-typedef struct QueueNode {
+
+
+//
+//		연결된 큐 정의
+// 
+// 연결된 큐는 다음과 같이 정의된다.
+// 
+//
+typedef int element;		// 요소의 타입
+
+typedef struct QueueNode {	// 큐의 노드의 타입
 	element data;
 	struct QueueNode* link;
 }QueueNode;
 
-typedef struct {
+typedef struct {			// 큐 ADT 구현
 	QueueNode* front, * rear;
 }LinkedQueueType;
 
+
+// 큐의 초기화 함수
 void init(LinkedQueueType* q) {
 	q->front = q->rear = 0;
 }
 
+
+// 공백 상태 검출 함수
 int is_empty(LinkedQueueType* q) {
 	return(q->front == NULL);
 }
 
+
+// 포화 상태 검출 함수
 int is_full(LinkedQueueType* q) {
 	return 0;
 }
 
+
+//
+//			삽입 연산
+// 
+// 삽입 연산은 먼저 동적 메모리 할당을 통하여 새로운 노드를 생성한 다음, 데이터를 저장하고
+// 연결 리스트의 끝에 새로운 노드를 추가하면 된다.
+// 
+// 만약 큐가 공백상태이면 front와 rear 모두 새로운 노드를 가리키도록 해야 한다. 만약 공백상태가
+// 아니고 기존의 노드가 있는 경우라면 rear가 가리키고 있는 노드의 링크 필드와 rear를 새로운 노드를
+// 가리키도록 변경하면 된다.
+// 
+// 
+//
+
+
+// 삽입 함수
 void enqueue(LinkedQueueType* q, element data) {
 
 	QueueNode* temp = (QueueNode*)malloc(sizeof(QueueNode));
@@ -42,6 +88,25 @@ void enqueue(LinkedQueueType* q, element data) {
 	}
 }
 
+
+//
+//			삭제 연산
+// 
+// 삭제 연산은 연결 리스트의 처음에서 노드를 꺼내오면 된다. 삭제 연산은
+// 먼저 큐가 공백상태인가를 검사해야 한다. 만약 공백상태라면 당연히 오류가 된다.
+// 현재 구현에서는 오류이면 오류 메시지를 출력하고 종료하도록 되어 있다.
+// 만약 공백상태가 아니라면 front가 가리키는 노드를 temp가 가리키도록 하고, front는
+// front의 링크값으로 대입한다. 그러면 front는 현재 가리키는 노드의 다음 노드를 
+// 가리키게 될 것이다. 
+// 
+// 그런 다음, temp가 가리키는 노드로부터 데이터를 꺼내오고 동적 메모리 해제를 통하여 이 노드를
+// 삭제하면 된다.
+// 
+//
+
+
+
+// 삭제 함수
 element dequeue(LinkedQueueType* q) {
 	QueueNode* temp = q->front;
 	element data;
@@ -72,6 +137,19 @@ void print_queue(LinkedQueueType* q) {
 	printf("NULL\n");
 }
 
+
+
+//
+//			연결된 큐 프로그램
+// 
+// 다음은 연결된 큐를 사용하는 전체 프로그램이다. 공백 상태 검출은 front이나 rear가 NULL 이면
+// 공백 상태로 판단할 수 있다. 연결 리스트의 경우, 메모리 할당과정에서 오류가 있지 않는 한 포화
+// 상태는 없다고 보아야 한다.
+// 
+// 
+//
+
+// 연결된 큐 테스트 함수
 int main(void) {
 	LinkedQueueType queue;
 	init(&queue);
--- 07_ArrayList/program7.3/program7-3.cpp
+++ 07_ArrayList/program7.3/program7-3.cpp
@@ -1,5 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
+//
+// 7.1 원형 연결 리스트
+// 
+//		원형 연결 리스트의 소개
+// 
+// 원형 연결 리스트란 마지막 노드가 첫 번재 노드를 가리키는 리스트이다. 즉 마지막 노드의 링크 필드가 널(NULL)이 아니라
+// 첫 번째 노드 주소가 되는 리스트이다. 어떤 장점이 있을까? 원형 연결 리스트에는 하나의 노드에서 다른 노드로의 접근이
+// 가능하다. 하나의 노드에서 링크를 계속 따라가면 결국 모든 노드를 거쳐 자기 자신으로 되돌아 올 수 있다는 것이다.
+// 
+// 삭제나 삽입 시에는 항상 선행 노드를 가리키는 포인터가 필요함을 기억하라.
+// 
+// 
+// 원형 연결 리스트가 특히 유용한 경우는 리스트의 끝에 노드를 삽입하는 연산이 단순 연결 리스트보다
+// 효율적일 수 있다는 것이다. 단순 연결 리스트에서 리스트 끝에 노드를 추가하려면 첫 번째 노드에서부터
+// 링크를 따라서 노드의 개수만큼 진행하여 마지막 노드까지 가야한다. 그러나 만약 원형 연결 리스트에서 다음과
+// 같이 헤드 포인터가 마지막 노드를 가리키도록 구성한다면 상수 시간 안에 처음과 끝에 노드를 삽입할 수 있어야 한다.
+// 
+// 이 변형된 원형 연결 리스트에서 마지막 노드는 헤드 포인터인 head가 가리키고 있고, 첫 번째 노드는 head->link가 가리키고
+// 있으므로, 리스트의 마지막에 노드를 삽입하거나 삭제하기 위하여 맨 끝까지 힘들게 가지 않아도 된다.
+// 
+// 
 
 typedef int element;
 typedef struct ListNode {
@@ -7,6 +28,67 @@ typedef struct ListNode {
 	struct ListNode* link;
 }ListNode;
 
+
+// 
+//		원형 리스트에 처음에 삽입
+// 
+// 
+// 이 변형된 원형 연결 리스트를 이용하여 리스트의 처음에 삽입하는 함수를 작성하여 보자 . 먼저 새로운 노드의 링크인
+// node->link가 첫 번째 노드를 가리키게 하고 다음에 마지막 노드의 링크가 node를 가리키게 하면 된다. 헤드 포인터인
+// head가 마지막 노드를 가리키는 것만 기억하면 소스를 이해하는데 별 문제가 없을 것이다.
+// 
+ListNode* insert_first(ListNode* head, element data) {
+
+	ListNode* node = (ListNode*)malloc(sizeof(ListNode));
+	node->data = data;
+	if (head == NULL) {
+		head = node;
+		node->link = head;
+	}
+	else {
+		node->link = head->link;
+		head->link = node;
+	}
+
+	return head;
+}
+
+//
+//			원형 리스트 끝에 삽입
+// 
+// 앞의 코드에 한 문장만 추가하면 원형 연결 리스트의 끝에 삽입할 수 있다. 즉 원형 연결 리스트는 어차피 원형으로 연결되어 있으므로 어디가 처음이고 어디가 끝인지는
+// 불분명하다. 따라서 head의 위치만 새로운 노드로 바꾸어주면 새로운 노드가 마지막 노드가 된다.
+// 
+//
+ListNode* insert_last(ListNode* head, element data) {
+
+	ListNode* node = (ListNode*)malloc(sizeof(ListNode));
+	node->data = data;
+
+	if (head == NULL)
+	{
+		head = node;
+		node->link = head;
+	}
+	else {
+		node->link = head->link;
+		head->link = node;
+		head = node;
+	}
+
+	return head;
+
+}
+
+//
+// 
+// 테스트 프로그램
+// 
+// 위의 삽입 함수들을 테스트하는 프로그램을 프로그램 7.3에 보였다. 원형 리스트가 이와 같은 장점이 있지만 마지막 노드의 링크가
+// NULL이 아니기 때문에 리스트의 끝에 도달했는지를 검사하려면 헤드 포인터와 비교해야 한다. 또한 while 루프 대신에 do-while 루프
+// 를 쎠야 함을 유의해야 한다.
+// 
+//
 void print_list(ListNode* head) {
 
 	ListNode* p;
@@ -24,41 +106,8 @@ void print_list(ListNode* head) {
 	printf("%d-> ", p->data);
 }
 
-ListNode* insert_first(ListNode* head, element data) {
 
-	ListNode* node = (ListNode*)malloc(sizeof(ListNode));
-	node->data = data;
-	if (head == NULL) {
-		head = node;
-		node->link = head;
-	}
-	else {
-		node->link = head->link;
-		head->link = node;
-	}
 
-	return head;
-}
-
-ListNode* insert_last(ListNode* head, element data) {
-	
-	ListNode* node = (ListNode*)malloc(sizeof(ListNode));
-	node->data = data;
-
-	if (head==NULL)
-	{
-		head = node;
-		node->link = head;
-	}
-	else {
-		node->link = head->link;
-		head->link = node;
-		head = node;
-	}
-
-	return head;
-
-}
 
 int main(void) {
 	ListNode* head = NULL;
